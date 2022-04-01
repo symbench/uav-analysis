b0209273996b21dccfecf20184d0f0fee2f59f50
sandbornm <michael.sandborn@vanderbilt.edu>
2022-04-01 13:49:20 -0500

    left some files behind

--- uav_analysis/mass_properties_hackathon2.py
+++ uav_analysis/mass_properties_hackathon2.py
@@ -14,125 +14,146 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from dataclasses import dataclass
-import numpy as np
+from typing import List
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sympy
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import csv 
-import os
-import pandas as pd
-
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
-"""
-Predict the mass properties of a wing given its geometry parameters
+from uav_analysis.fdm_review import FdmReport
+from uav_analysis import func_approx as fa
+from uav_analysis.mass_properties_hackathon1 import (inertia_matrix, translate_inertia)
 
-inputs: dictionary of {var_name: [values]} to predict the outputs
-outputs: dictionary of {var_name: [values]} of target outputs
 
-"""
-class WingPredictor():
-	def __init__(self, 
-	inputs, 
-	outputs,
-	in_vars= ["chord", "span", "max_load", "thickness"],
-	out_vars= ["coordIxx", "coordIxz", "coordIyy", "coordIzx", "coordIzz", "cgIxx", "cgIyy", "cgIzz"]):
-		assert isinstance(inputs, dict)
-		assert isinstance(outputs, dict)
-		self.inputs = inputs
-		self.outputs = outputs
-		self.in_vars = in_vars
-		self.out_vars = out_vars
+class MassPropertyDataset():
+	def __init__(self, component_name: str, data_path: str, input_names: list, output_names: list, target: str):
 
-	def select_features(self, var_idx):
-		self.in_features = np.vstack([self.inputs[var] for var in self.in_vars]).T  # (152, 4)
-		self.out_features = np.vstack([self.outputs[self.out_vars[var_idx]]]).T  # (152, 1)
-		print(f"in_features shape: {self.in_features.shape}")
-		print(f"out_features shape: {self.out_features.shape}")
+		self.component_name = component_name
+		self.data_path = data_path
+		self.input_names = input_names
+
+		self.output_names = output_names
+		assert target in self.output_names
+		self.target = target
+		self.output_names = self.output_names
+		self.load_data()
+		#self.scale_features()
+
+	
+	def load_data(self):
+		# read all csvs into a single dataframe to initialize the MassPropertyDataset
+		massprops_files = os.listdir(self.data_path)
+		self.df = pd.concat([pd.read_csv(os.path.join(self.data_path, f)) for f in massprops_files])
+		stem = "Component_param_"
+		self.invar_keys = [stem + iv for iv in self.input_names]
+
+		self.input_df = self.df[[ik for ik in self.invar_keys]]
+		self.input_dict = {k: self.input_df[[k]].values for k in self.invar_keys}
+		
+		self.inputs_data = self.input_df.values
+
+		self.output_df = self.df[[ok for ok in self.output_names]]
+		self.output_dict = self.output_df.to_dict()
+		self.target_data = self.output_df[[self.target]].values
+		
+
+		print(f"type input data: {type(self.inputs_data)}, shape: {self.inputs_data.shape}")
+		print(f"type target data: {type(self.target_data)}, shape: {self.target_data.shape}")
+
+	
+	def update_target(self, new_target: str):
+		assert new_target in self.output_names
+		print(f"changing target {self.target} --> {new_target}")
+		self.target = new_target
+		self.target_data = self.output_df[[self.target]].values
+		# print(f"updated target to {self.target} to {self.target_data[:5]}")
+
 
 	def scale_features(self):
-		scaler = StandardScaler()
-		self.in_scaled = scaler.fit_transform(self.in_features)
-		self.out_scaled = scaler.fit_transform(self.out_features)
+		scaler = StandardScaler() #  center to zero mean and unit variance
+		self.in_scaled = scaler.fit_transform(self.inputs)
+		self.out_scaled = scaler.fit_transform(self.outputs)
+		print(f"{self.component_name} scaled [{type(self.in_scaled)}, {self.in_scaled.shape}]")
+		print(f"{self.component_name} scaled [{type(self.out_scaled)}, {self.out_scaled.shape}]")
+
+	def output_head(self):
+		print(self.output_df.head())
+
+
+	def viz(self, input_trio: list, output_trio: list):
+		# plot specified features in 3D (side by side subplots from inputs to outputs)
+		pass
+
+
+"""
+
+Predict the mass properties of a UAM component given its geometry parameters from Creo
+
+"""
+class MassPropertyPredictor():  # updated version will take MassPropertyDataset as input
+	def __init__(self, name: str, dataset: MassPropertyDataset):
+		self.name = name
+		self.dataset = dataset
+		self.predictors = {}
 
 	def fit(self):
-		self.regs = {ov: None for ov in self.out_vars}
-		print(self.regs)
-		for i in range(len(self.out_vars)):
-			print(f"fitting input: {self.in_vars} to {self.out_vars[i]}")
-			self.select_features(i)
-			self.scale_features()
-			in_train, in_test, out_train, out_test = train_test_split(self.in_scaled, self.out_scaled, test_size=0.10, random_state=42)
-			reg = LinearRegression().fit(in_train, out_train)
-			cod = reg.score(in_test, out_test)
-			print(f"test score: {cod}")
-			print(f"coefficients: {reg.coef_}")
-			print(f"intercept: {reg.intercept_}")
-			self.regs[self.out_vars[i]] = (reg, reg.score(in_test, out_test))
-	
-	def predict(self, inputs, out_var_name):
-		assert isinstance(inputs, np.ndarray)
-		assert inputs.shape[1] == len(self.in_vars)
-		assert out_var_name in self.out_vars
-		return self.regs[out_var_name].predict(inputs)
+		#  todo maybe modify this expression for different targets
+		#  todo maybe visualize results
+		#  ax^3 + bx^2 + cx + d
+		a = sympy.Symbol('a')
+		b = sympy.Symbol('b')
+		c = sympy.Symbol('c')
+		d = sympy.Symbol('d')
+		x = sympy.Symbol('x')
+		f = a * (x ** 3) + b * (x ** 2) + c * x + d
+
+		print(f"target data {self.dataset.target_data} shape {self.dataset.target_data.shape}")
+		for ov in self.dataset.output_names:
+			self.dataset.update_target(ov)
+			subs, error = fa.approximate(f, self.dataset.input_dict, self.dataset.target_data.ravel())
+			self.predictors[ov] = (subs, error)
+
 
 	def viz(self):
-		x_labels = self.out_vars
-		vals = sorted([self.regs[ov][1] for ov in self.out_vars])
-		plt.xlabel(x_labels)
-		plt.ylabel("R^2 value")
-		plt.plot(vals)
-		plt.title("LinReg - mass properties and determination coefficients")
-		plt.show()
-
-def run(data_path = os.path.join(os.path.abspath(os.getcwd()), "uav_analysis", "data_hackathon2_uam")):
-
-	# input data 
-	wing_analysis_files = [f for f in os.listdir(data_path) if "wing_analysis_pareto" in f]
-	#  output data
-	massprops_data_path = os.path.join(data_path, "wing_massprops")
-	wing_massprops_files = [f for f in os.listdir(massprops_data_path) if "_pareto.csv" in f]
-
-	wingprops = ["chord", "span", "max_load", "thickness"]
-	input_data = {wp: [] for wp in wingprops}
-
-	for data_file in wing_analysis_files:
-		df = pd.read_csv(os.path.join(data_path, data_file))
-		for wp in input_data.keys():
-			if wp != "thickness":
-				input_data[wp].extend(df[wp].values)
-		
-		pct_thickness = np.array([.01 * int(str(x.split(" ")[1][-2:])) for x in df['profile'].values])
-		input_data["thickness"].extend(df['chord'].values * pct_thickness)
-
-	# convert lists to numpy arrays
-	for prop, data in input_data.items():
-		input_data[prop] = np.array(data)
-
-	# output data
-	massprops = ["coordIxx", "coordIxz", "coordIyy", "coordIzx", "coordIzz", "cgIxx", "cgIyy", "cgIzz"]
-
-	output_data = {mp: [] for mp in massprops}	
-
-	for data_file in wing_massprops_files:
-		df = pd.read_csv(os.path.join(massprops_data_path, data_file))
-		#print(df)
-
-		for mp in output_data.keys():
-			output_data[mp].extend(df[mp].values)
-
-	for prop, data in output_data.items():
-		output_data[prop] = np.array(data)
+		pass
 
 
-	# predict mass properties from wing geometry parameters
-	wp = WingPredictor(input_data, output_data)
+
+def run(data_path: str = os.path.join(os.path.abspath(os.getcwd()), "uav_analysis", "data_hackathon2_uam")):
+
+	wing_massprops_data_path = os.path.join(data_path, "wing_massprops")
+	wing_invar_names = ["CHORD_1", "SPAN", "THICKNESS"]
+	wing_outvar_names = ["coordIxx","coordIxz","coordIyy","coordIzx","coordIzz", "cgIxx", "cgIyy", "cgIzz"]
+	wing_dataset = MassPropertyDataset("wing", wing_massprops_data_path, wing_invar_names, wing_outvar_names, "coordIxx")
+	print(" ---- wing dataset ---- ")
+	for o in wing_outvar_names[1:]:
+		wing_dataset.update_target(o)
+	wing_dataset.output_head()
+	print(" ---------------------- ")
+
+	cylinder_massprops_data_path = os.path.join(data_path, "cylinder_massprops")
+	cylinder_invar_names = ["DIAMETER", "LENGTH", "PORT_THICKNESS"]
+	cylinder_outvar_names = ["coordIxx","coordIyy", "coordIzz", "cgIxx", "cgIyy", "cgIzz"]
+	cylinder_dataset = MassPropertyDataset("cylinder", cylinder_massprops_data_path, cylinder_invar_names, cylinder_outvar_names, "coordIxx")
+	print(" ---- cylinder dataset ---- ")
+	for o in cylinder_outvar_names[1:]:
+		cylinder_dataset.update_target(o)
+	cylinder_dataset.output_head()
+	print(" -------------------------- ")
+
+	wp = MassPropertyPredictor("wing", wing_dataset)
 	wp.fit()
-	wp.viz()
-
-
+	cp = MassPropertyPredictor("cylinder", cylinder_dataset)
+	cp.fit()
 if __name__ == "__main__":
 	#  path hack for direct calls to script during development
 	data_path = os.path.join(os.getcwd(), "data_hackathon2_uam")
+	
 	run(data_path)
