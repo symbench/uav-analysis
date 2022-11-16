b20ae4e27dc02460d4815535de33ca0c69f55eb0
Miklos Maroti <mmaroti@gmail.com>
2022-11-16 20:21:53 +0100

    updated copyright dates

--- uav_analysis/func_approx.py
+++ uav_analysis/func_approx.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2021, Miklos Maroti
+# Copyright (C) 2021-2022, Miklos Maroti
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- uav_analysis/motor_propeller_analysis.py
+++ uav_analysis/motor_propeller_analysis.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2021, Carlos Olea and Miklos Maroti
+# Copyright (C) 2021-2022, Carlos Olea and Miklos Maroti
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
